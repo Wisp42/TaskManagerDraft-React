59498e8d8130a33d49af1720834c5aef69615f72
Wisp <mr.wisp173.1.1@gmail.com>
2023-03-04 08:05:48 +0000

    Update 12 files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import TaskItem from "./components/TaskItem";
-import TaskList from "./components/TaskList";
-import CreateTaskForm from "./components/CreateTaskForm";
 import "./styles.css";
 import { useState } from "react";
 
 function App() {
-  const PlannerDataList = JSON.parse(localStorage.getItem("PlannerDataList"));
-  const [currentTaskList, setCurrentTaskList] = useState(PlannerDataList[0].data);
-  function checkFirstLaunch() {
-    if (localStorage.getItem("PlannerDataList") == null) {
-      localStorage.setItem(
-        "PlannerDataList",
-        JSON.stringify([
-          { ID: Date.now(), name: "My Tasks", type: "TaskList", data: [] }
-        ])
-      );
-    }
-  }
-
   return (
     <div className="App" onLoad={checkFirstLaunch()}>
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happen!jjj</h2>
       {/* <TaskItem title="Title" description="description" /> */}
-      <TaskList />
-      <CreateTaskForm list_ID={PlannerDataList[0].ID} />
     </div>
   );
 }
